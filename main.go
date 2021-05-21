6facd01b6de364c927cbc3d816e3dfaf8fc1f67e
Xizhou Feng <xizhou.feng@gmail.com>
2021-05-21 16:55:44 -0400

    add split by both queue and hostname

--- main.go
+++ main.go
@@ -3,19 +3,52 @@ package main
 import (
 	"flag"
 	"github.com/fengxizhou/pbslog"
+	"log"
+	"os"
 	"path/filepath"
 )
 
-var inDirFlag = flag.String("in_dir", "/var/spool/xdmod/palmetto", "the directory that stores the pbs accounting logs")
-var outDirFlag = flag.String("out_dir", "/var/spool/xdmod/skylight", "the directory that stores the filtered accounting logs")
-var hostPattern = flag.String("host_prefix", "^sky", "a regular expression string for the matched hostnames")
+var inDirFlag = flag.String("in_dir", "/var/spool/xdmod/palmetto/accounting", "the directory that stores the pbs accounting logs")
+var outDirFlag = flag.String("out_dir", "/var/spool/xdmod/skylight/accounting", "the directory that stores the filtered accounting logs")
+var hostPattern = flag.String("host_prefix",	"", "a regular expression string for the matched hostnames")
+var queuePattern = flag.String("queue_prefix", "", "a regular expression string for the matched queues")
+var logFile = flag.String("log_path", "/var/log/jobsplitter.log", "the log file path")
 
 func main() {
+	var (
+		logFH *os.File = nil
+		logger = log.Default()
+	)
+	logger.SetPrefix("pbslog: ")
+	logger.SetFlags(log.Ldate | log.Ltime)
+	if *logFile != "" {
+		var err
+		logFH, err = os.OpenFile(*logFile, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		logger.SetOutput(logFH)
+	}
+	defer func(){
+		if logFH != nil {
+			logFH.Close()
+		}
+	}()
+	
 	flag.Parse()
 
-	filter := pbslog.NewHostPrefixFilter("^sky")
 	filenamePattern := `\d{8}`
 	statesStorePath := filepath.Join(*outDirFlag, "states.json")
 	lfs := pbslog.NewLogFileStates(*inDirFlag, filenamePattern, statesStorePath)
-	pbslog.FilterLogsBatch(lfs, *outDirFlag, filter)
+
+	var filters []pbslog.JobFilter
+	if *hostPattern != "" {
+		filters = append(filters, pbslog.NewHostPrefixFilter(*hostPattern))
+	}
+	if *queuePattern != "" {
+		filters = append(filters, pbslog.NewQueueFilter(*queuePattern))
+	}
+	if len(filters) >= 1 {
+		pbslog.FilterLogsBatch(lfs, *outDirFlag, filters)
+	}
 }
